9bd9d012bff90474d2ae105c402f989cb900d196
atronah <atronah.ds@gmail.com>
2021-01-23 20:26:32 +0300

    ft(bot): adds main script

---
+++ mutex_bot.py
@@ -0,0 +1,111 @@
+import collections
+import os
+
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, MessageHandler
+from telegram.ext import Filters
+import yaml
+import logging, logging.config
+import sys
+import threading
+
+
+# default settings
+settings = {
+    'access': {
+        'token': None
+    },
+    'logging': {
+        'version': 1.0,
+        'formatters': {
+            'default': {
+                'format': '[{asctime}]{levelname: <5}({name}): {message}',
+                'style': '{'
+            }
+        },
+        'handlers': {
+            'general': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'level': 'DEBUG',
+                'filename': 'bot.log',
+                'formatter': 'default'
+            },
+            'stdout': {
+                'class': 'logging.StreamHandler',
+                'level': 'INFO',
+                'formatter': 'default'
+            },
+            'unknown_messages': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'level': 'DEBUG',
+                'filename': 'unknown_messages.log',
+                'formatter': 'default'
+            }
+        },
+        'loggers': {
+            'unknown_messages': {
+                'level': 'DEBUG',
+                'handlers': ['unknown_messages']
+            }
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['general']
+        },
+    }
+}
+
+
+
+def recursive_update(target_dict, update_dict):
+    if not isinstance(update_dict, dict):
+        return target_dict
+    for k, v in update_dict.items():
+        if isinstance(v, collections.abc.Mapping):
+            target_dict[k] = recursive_update(target_dict.get(k, {}), v)
+        else:
+            target_dict[k] = v
+    return target_dict
+
+if os.path.exists('conf.yml'):
+    with open('conf.yml', 'rt') as conf:
+        recursive_update(settings, yaml.safe_load(conf))
+else:
+    with open('conf.yml', 'wt') as conf:
+        yaml.dump(settings, conf)
+
+
+logging.config.dictConfig(settings['logging'])
+
+
+if not settings['access']['token']:
+    logging.error('Empty bot token in conf.yml (`access/token`)')
+    sys.exit(1)
+
+
+updater = Updater(token=settings['access']['token'], use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    user = update.effective_user
+    chat = update.effective_chat
+    update.message.reply_markdown(f'Hello, {user.username}!\n'
+                                  f'Your user ID is `{user.id}`'
+                                  f' and our    chat ID is `{chat.id}`')
+
+
+def message_logger(update, context):
+    logger = logging.getLogger('unknown_messages')
+    logger.debug(f'{update.effective_user.id} {update.message.text}')
+    update.message.reply_text("I don't understand what you mean, that's why I've logged your message")
+
+
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.all, message_logger))
+
+
+logging.info('start polling...')
+updater.start_polling()
+updater.idle()
