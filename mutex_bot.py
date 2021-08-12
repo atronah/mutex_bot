cf5d447b3090c4cd770a136e78cf3571b9a89091
atronah <atronah.ds@gmail.com>
2021-08-12 21:12:54 +0300

    fx(bot): fixes error handling
    use `send_message` instead `reply_text` because not always there is a
    message to reply (when exception is raising during keyboard callback
    processing)

--- mutex_bot.py
+++ mutex_bot.py
@@ -244,7 +244,11 @@ def build_keyboard(update: Update, context: CallbackContext) -> InlineKeyboardMa
 
 
 def error_handler(update: Update, context: CallbackContext):
-    update.message.reply_text(tr(context, 'common.internal_exception', exception_info=str(context.error)))
+    exception_info = str(context.error)
+    # import traceback
+    # exception_info += os.linesep
+    # exception_info += traceback.format_exc()
+    context.bot.sendMessage(update.effective_chat.id, tr(context, 'common.internal_exception', exception_info=exception_info))
     raise context.error
 
 
