edf6c1ae6b5aed3181bafe4870c1c47d3b6887db
atronah <atronah.ds@gmail.com>
2022-08-26 09:59:30 +0300

    ft(re-run-bot): updates run script
    - swith to using console_scripts entry point
    - add using python 3.7 instead python3 if exists (because python-telegram-bot falls on python 3.6.6 with error `TypeError: __dict__ slot disallowed: we already got one`)

--- re-run-bot.sh
+++ re-run-bot.sh
@@ -2,24 +2,24 @@
 
 script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# " <- extra double quote to fix incorect parsing nested double quotes in previous command by some editors
-
-
-# full path to prevent killing any other bots with `bot.py` main script
-run_command="python3 ${script_path}/mutex_bot.py"
-
 pkill -f "${run_command}"
 
 pushd "${script_path}"
 
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    if ! command -v python3.7 &> /dev/null
+    then
+        python3.7 -m venv venv
+    else
+        python3 -m venv venv
+    fi
 fi
 
 source ./venv/bin/activate
 pip install --upgrade pip setuptools wheel
 pip install .
 
-nohup "${run_command}" &
+
+nohup mutex_bot &
 
 popd
