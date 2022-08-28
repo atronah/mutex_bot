785a27b6de2ce94543e8bbba391beffcbf4d7a4f
atronah <atronah.ds@gmail.com>
2022-08-28 20:18:52 +0300

    rf(bot): reformat source code

--- mutex_bot/bot.py
+++ mutex_bot/bot.py
@@ -85,7 +85,6 @@ def recursive_update(target_dict, update_dict):
     return target_dict
 
 
-
 def build_keyboard(update: Update, context: CallbackContext) -> InlineKeyboardMarkup:
     if context.user_data.get('mode', STANDARD_USER_MODE) == REMOVING_USER_MODE:
         rows = []
@@ -120,7 +119,8 @@ def error_handler(update: Update, context: CallbackContext):
     import traceback
     exception_info += os.linesep
     exception_info += traceback.format_exc()
-    context.bot.sendMessage(update.effective_chat.id, tr(context, 'common.internal_exception', exception_info=exception_info))
+    context.bot.sendMessage(update.effective_chat.id,
+                            tr(context, 'common.internal_exception', exception_info=exception_info))
     raise context.error
 
 
