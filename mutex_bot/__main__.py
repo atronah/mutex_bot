550d9657f98068aacd8c80e120c77d733b2d1dfd
atronah <atronah.ds@gmail.com>
2023-10-16 07:23:48 +0300

    wip

--- mutex_bot/__main__.py
+++ mutex_bot/__main__.py
@@ -1,55 +1,31 @@
-import logging
-import os
-import sys
+import logging.config
+import os.path
 
-import i18n
 import yaml
-from telegram.ext import PicklePersistence, Updater, CommandHandler, Filters, CallbackQueryHandler, MessageHandler
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder, CommandHandler, Defaults
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from mutex_bot.bot import recursive_update, settings, error_handler, help_command, start, add_resource, remove_resource, \
-    export_chat_data, import_chat_data, finish, lang, force_cleanup, other_messages, button
+from mutex_bot.bot import start, help_handler
 
-if os.path.exists('conf.yml'):
-    with open('conf.yml', 'rt') as conf:
-        recursive_update(settings, yaml.safe_load(conf))
-else:
-    with open('conf.yml', 'wt') as conf:
-        yaml.dump(settings, conf)
+CONF_FILENAME = 'conf.yaml'
 
-logging.config.dictConfig(settings['logging'])
+logging.getLogger().setLevel(logging.INFO)
 
-if not settings['access']['token']:
-    logging.error('Empty bot token in conf.yml (`access/token`)')
-    sys.exit(1)
-
-if not settings['persistence']['filename']:
-    logging.error('Empty filename fot persistence in conf.yml (`persistence/filename`)')
-    sys.exit(1)
+with open(CONF_FILENAME, 'rt') as conf:
+    config = yaml.safe_load(conf)
 
 
-pp = PicklePersistence(settings['persistence']['filename'])
-updater = Updater(token=settings['access']['token'], persistence=pp, use_context=True)
-dispatcher = updater.dispatcher
+engine = create_engine('sqlite:///:memory:', echo=True)
+sessionmaker = session_maker(engine)
 
+# logging.config.dictConfig(config['logging'])
+# defaults = Defaults(parse_mode=ParseMode.MARKDOWN_V2)
+bot_token = config['access']['bot_token']
+application = ApplicationBuilder().token(bot_token).build()
 
-dispatcher.add_error_handler(error_handler)
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('help', help_command))
-dispatcher.add_handler(CommandHandler('add_resource', add_resource))
-dispatcher.add_handler(CommandHandler('remove_resource', remove_resource))
-dispatcher.add_handler(CommandHandler('export_chat_data', export_chat_data))
-dispatcher.add_handler(CommandHandler('import_chat_data', import_chat_data))
-dispatcher.add_handler(CommandHandler('finish', finish))
-dispatcher.add_handler(CommandHandler('lang', lang))
-dispatcher.add_handler(CommandHandler('force_cleanup', force_cleanup))
-dispatcher.add_handler(MessageHandler(Filters.all & ~Filters.status_update, other_messages))
-dispatcher.add_handler(CallbackQueryHandler(button))
+application.add_handler(CommandHandler('start', start))
+application.add_handler(CommandHandler('help', help_handler))
 
-
-i18n.load_path.append('i18n')
-i18n.set('fallback', 'en')
-
-
-logging.info('start polling...')
-updater.start_polling()
-updater.idle()
+application.run_polling()
